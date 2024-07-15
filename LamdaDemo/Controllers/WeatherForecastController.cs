02cb95a2c2007a75cf3ba364e193e0acd2ccd822
Mani <rmanikandaraja@gmail.com>
2024-07-15 17:02:49 +0530

    Updated

--- LamdaDemo/Controllers/WeatherForecastController.cs
+++ LamdaDemo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace LamdaDemo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezinggggggg", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
